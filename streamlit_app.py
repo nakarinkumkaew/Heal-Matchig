f8c702a7d2efdf0bacc0b75733b0904bd82478ed
nakarinkumkaew <87959444+nakarinkumkaew@users.noreply.github.com>
2024-08-05 21:11:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,9 @@ import os
 import random
 
 def get_random_image_path(folder_path):
-    # List all files in the folder
     all_files = os.listdir(folder_path)
-    # Filter out non-image files (optional)
     image_files = [f for f in all_files if f.endswith(('png', 'jpg', 'jpeg', 'gif'))]
-    # Select a random image file
     random_image_file = random.choice(image_files)
-    # Return the full path to the random image file
     return os.path.join(folder_path, random_image_file)
 
 def go_to_page(page_name):
@@ -22,21 +18,17 @@ def go_to_page(page_name):
 def toggle_drawing():
     st.session_state.draw_enabled = not st.session_state.draw_enabled
 
-# Function to display icon with a link within content
-def display_icon_with_link(icon_path,content, url):
-    # Load and encode the icon image
+def display_icon_with_link(icon_path, content, url):
     with open(icon_path, "rb") as icon_file:
         icon_encoded = base64.b64encode(icon_file.read()).decode()
-    
     icon_and_content_html = f"""
     <div style="display: flex; align-items: center; margin-bottom: 10px;">
         <a href="{url}" target="_blank">
-            <img src="data:image/png;base64,{icon_encoded}" style="height: 50px; margin-right: 20px;">
+            <img src="data:image/png;base64,{icon_encoded}" style="height: 30px; margin-right: 10px;">
         </a>
-        <span style="font-size: 1 em; color: #333333;">{content}</span>
+        <span style="font-size: 1em; color: #333333;">{content}</span>
     </div>
     """
-    
     st.markdown(icon_and_content_html, unsafe_allow_html=True)
 
 def set_background(image_file, background_size="cover"):
@@ -83,18 +75,15 @@ def set_background(image_file, background_size="cover"):
             color: #333333;
         }}
         .shift-down {{
-            margin-top: 30px; 
+            margin-top: 20px; 
         }}
         </style>
         """,
         unsafe_allow_html=True
     )
 
-
-# Set the background image
 set_background('back_ground.png', background_size="70% 100%")
 
-# Initialize session state variables if not already set
 if 'draw_enabled' not in st.session_state:
     st.session_state.draw_enabled = False
 
@@ -102,25 +91,21 @@ if 'current_page' not in st.session_state:
     st.session_state.current_page = "main"
 
 if st.session_state.current_page == "main":
-    col1, col2 = st.columns(2)
+    st.markdown('<div class="shift-down">ตอนนี้คุณเป็นอย่างไรบ้าง?</div>', unsafe_allow_html=True)
+    if st.button('กำลังเหนื่อยอยู่ใช่มั๊ย', key='1-1', on_click=go_to_page, args=("tried",)):
+        pass
+    if st.button('กำลังกังวลอยู่ใช่มั๊ย', key='1-2', on_click=go_to_page, args=("confuse",)):
+        pass
+    if st.button('ต้องการกำลังใจมั๊ย', key='1-3', on_click=go_to_page, args=("value",)):
+        pass
 
-    with col1:
-        st.markdown('<div class="shift-down">ตอนนี้คุณเป็นอย่างไรบ้าง?</div>', unsafe_allow_html=True)
-        if st.button('กำลังเหนื่อยอยู่ใช่มั๊ย', key='1-1', on_click=go_to_page, args=("tried",)):
-            pass
-        if st.button('กำลังกังวลอยู่ใช่มั๊ย', key='1-2', on_click=go_to_page, args=("confuse",)):
-            pass
-        if st.button('ต้องการกำลังใจมั๊ย', key='1-3', on_click=go_to_page, args=("value",)):
-            pass
-
-    with col2:
-        st.markdown('<div class="shift-down">อยากรู้จักตัวเองมากขึ้นมั๊ย?</div>', unsafe_allow_html=True)
-        if st.button('อยากรู้จุดเด่นของฉันจัง', key='3-1'):
-            st.write("landmark")
-        if st.button('ฉันมีความสามารถอะไรบ้าง?', key='3-2'):
-            st.write("ability")
-        if st.button('ฉันเหมาะกับอาชีพอะไร?', key='3-3'):
-            st.write("job?")
+    st.markdown('<div class="shift-down">อยากรู้จักตัวเองมากขึ้นมั๊ย?</div>', unsafe_allow_html=True)
+    if st.button('อยากรู้จุดเด่นของฉันจัง', key='3-1'):
+        st.write("landmark")
+    if st.button('ฉันมีความสามารถอะไรบ้าง?', key='3-2'):
+        st.write("ability")
+    if st.button('ฉันเหมาะกับอาชีพอะไร?', key='3-3'):
+        st.write("job?")
 
     st.write("คุณมีอะไรอยากระบายมั๊ย?")
     if st.button('ลองวาดออกมาดูสิ' if not st.session_state.draw_enabled else 'ฉันรู้สึกดีขึ้นแล้ว', key='draw-toggle', on_click=toggle_drawing):
@@ -128,12 +113,12 @@ if st.session_state.current_page == "main":
 
     if st.session_state.draw_enabled:
         canvas_result = st_canvas(
-            fill_color="rgba(255, 165, 0, 0.3)",  # Fill color with some opacity
+            fill_color="rgba(255, 165, 0, 0.3)",
             stroke_width=1,
             stroke_color="#000000",
             background_color="#ffffff",
             height=400,
-            width=720,  # Adjusted width to remove the marked area
+            width=300,
             drawing_mode="freedraw",
             key="canvas",
         )
@@ -150,17 +135,22 @@ if st.session_state.current_page == "main":
                 file_name="drawing.png",
             )
 
-# Sub page content
-elif st.session_state.current_page in ("tried","confuse","value"):
-    image_path = get_random_image_path(".devcontainer/card/"+st.session_state.current_page)
+elif st.session_state.current_page in ("tried", "confuse", "value"):
+    image_path = get_random_image_path(".devcontainer/card/" + st.session_state.current_page)
     image = Image.open(image_path)
     st.markdown('<div class="shift-down">สามารถบันทึกรูปภาพ เพื่อแชร์ให้คนสำคัญของเพื่อนๆ และให้กำลังใจตัวเองได้นะ</div>', unsafe_allow_html=True)
     st.image(image)
     st.write("ถ้าเพื่อนต้องการคนรับฟัง หรืออยากเล่าอะไร น้องยินดีรับฟังเสมอนะ")
     st.write("สามารถส่งข้อความ หรือติดตามผลงานของน้องยินดีได้ตามช่องทางนี้เลย")
-    display_icon_with_link(".devcontainer/icon/facebook.png",'The YouDee Project',"https://www.facebook.com/youdee.project")
-    display_icon_with_link(".devcontainer/icon/ig.png",'@youdee.project',"https://www.instagram.com/youdee.project/")
-    display_icon_with_link(".devcontainer/icon/web.png",'The YouDee Project',"https://www.youdee.redcross.or.th/")
+    display_icon_with_link(".devcontainer/icon/facebook.png", 'The YouDee Project', "https://www.facebook.com/youdee.project")
+    display_icon_with_link(".devcontainer/icon/ig.png", '@youdee.project', "https://www.instagram.com/youdee.project/")
+    display_icon_with_link(".devcontainer/icon/web.png", 'The YouDee Project', "https://www.youdee.redcross.or.th/")
     if st.button("Go Back", on_click=go_to_page, args=("main",)):
         pass
 
+elif st.session_state.current_page == "landmark":
+    st.write("ถ้าเพื่อนต้องการคนรับฟัง หรืออยากเล่าอะไร น้องยินดีรับฟังเสมอนะ")
+    st.write("สามารถส่งข้อความ หรือติดตามผลงานของน้องยินดีได้ตามช่องทางนี้เลย")
+    display_icon_with_link(".devcontainer/icon/facebook.png", 'The YouDee Project', "https://www.facebook.com/youdee.project")
+    display_icon_with_link(".devcontainer/icon/ig.png", '@youdee.project', "https://www.instagram.com/youdee.project/")
+    display_icon_with_link(".devcontainer/icon/web.png", 'The YouDee Project', "https://www.y
